a03827b4497300bf3172ac6046ff313647229c4f
imdayoung <dlekdud0102@naver.com>
2024-06-11 17:39:17 +0900

    feat: add sidebar, login, setting view

--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,11 +1,75 @@
 <template>
-
+<div class="container-fluid">
+    <div class="row flex-nowrap">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-blue">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <div class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-black text-decoration-none">
+                    <img src="@/assets/logo_badge.png" width="60" alt="">
+                    <span class="fs-5 d-none d-sm-inline">호주머니</span>
+                </div>
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li class="nav-item">
+                        <div class="mt-3">
+                            <RouterLink to="/home" class="nav-link align-middle px-0 ms-1 d-none d-sm-inline text-black fw-bold">Home</RouterLink>
+                        </div>
+                    </li>
+                    <li class="nav-item mt-3">
+                        <span class=" nav-link px-0 align-middle ms-1 d-none d-sm-inline text-black fw-bold">수입/지출</span>
+                        <ul class="collapse show nav flex-column ms-1" id="submenu1">
+                            <li class="mt-2">
+                                <RouterLink to="/come/income" class="nav-link align-middle px-0 ms-1 d-none d-sm-inline text-black">수입 내역</RouterLink>
+                            </li>
+                            <li class="mt-2">
+                                <RouterLink to="/come/outcome" class="nav-link align-middle px-0 ms-1 d-none d-sm-inline text-black">지출 내역</RouterLink>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item mt-3">
+                        <span class=" nav-link px-0 align-middle ms-1 d-none d-sm-inline text-black fw-bold">분석 리포트</span>
+                        <ul class="collapse show nav flex-column ms-1" id="submenu1">
+                            <li class="mt-2">
+                                <RouterLink to="/chart/category" class="nav-link align-middle px-0 ms-1 d-none d-sm-inline text-black">카테고리별 그래프</RouterLink>
+                            </li>
+                            <li class="mt-2">
+                                <RouterLink to="/chart/comes" class="nav-link align-middle px-0 ms-1 d-none d-sm-inline text-black">수입/지출 그래프</RouterLink>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <hr>
+                <div class="pb-4">
+                    <RouterLink to="/user/detail" class="d-flex align-items-center text-black text-decoration-none">
+                        <div>
+                            <img src="https://github.com/mdo.png" alt="profileImage" width="30" height="30" class="rounded">
+                        </div>
+                        <span class="d-none d-sm-inline mx-1">닉네임</span>
+                        <span>
+                            <RouterLink to="/user/setting" class="d-flex text-black text-decoration-none mx-3">설정</RouterLink>
+                        </span>
+                    </RouterLink>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col py-3">
+            <RouterView />
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
 
 </script>
 
-<style>
-
+<style scoped>
+.bg-blue {
+    background-color: #8AB2DC;
+}
+.bg-lightblue {
+    background-color: #BECEE8;
+}
+.bg-white {
+    background-color: white;
+}
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,75 @@
 <template>
-
+<div class="container d-flex justify-content-center align-items-center vh-100">
+    <div class="text-center">
+        <div>
+            <img src="@/assets/logo_badge.png" alt="logo" class="img-fluid logo-image" />
+        </div>
+        <div class="mb-3">
+            <span class="fs-4">Enter your email, password for this app</span>
+        </div>
+        <div class="mx-5">
+            <div class="mb-3">
+                <input type="text" class="form-control" v-model="userInfo.email" placeholder="email@domain.com">
+            </div>
+            <div class="mb-3">
+                <input type="password" class="form-control" v-model="userInfo.password">
+            </div>
+            <div class="d-grid gap-2 d-md-block">
+                <button type="button" class="btn btn-dark" @click="btnLogin">Login with email</button>
+            </div>
+        </div>
+    </div>
+</div>
 </template>
 
-<script>
+<script setup>
+import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+const router = useRouter();
 
+const userInfo = reactive({ email: "", password: "" });
+const userList = ref([]);
+
+const btnLogin = () => {
+    sessionStorage.setItem("email", "");
+    sessionStorage.setItem("password", "");
+
+    let user = userList.value.find((u) => u.email == userInfo.email);
+
+    if (userInfo.email == "") {
+        alert("아이디를 입력해주세요.");
+        return;
+    }
+
+    if (user == null) {
+        alert("해당하는 아이디가 없습니다.");
+        return;
+    }
+
+    if (user.password != userInfo.password) {
+        alert("패스워드가 일치하지 않습니다.");
+        return;
+    }
+
+    sessionStorage.setItem("email", user.email);
+    sessionStorage.setItem("password", user.password);
+
+    router.push({path: '/home'});
+}
+
+axios.get('http://localhost:3001/users')
+.then(res => {
+    userList.value = res.data;
+    console.log(userList.value);
+})
+.catch(err => {
+    console.error(err);
+});
 </script>
 
-<style>
-
+<style scoped>
+.logo-image {
+    width: 15rem;
+}
 </style>

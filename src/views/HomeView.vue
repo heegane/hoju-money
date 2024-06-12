f1f97d5ff15c5734773d1d0c16e8ab3e3f311780
lgh9776 <lgh9776@naver.com>
2024-06-12 13:27:42 +0900

    fix: Change home name

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
     <!-- <IncomeCreate/> -->
     <!-- <OutcomeCreate/> -->
-     <!-- <IncomeUpdate/> -->
+    <!-- <IncomeUpdate/> -->
 </template>
 
 <script setup>
     // import IncomeCreate from '@/components/transaction/IncomeCreate.vue';
     // import OutcomeCreate from '@/components/transaction/OutcomeCreate.vue'
-    //import IncomeUpdate from '@/components/transaction/IncomeUpdate.vue';
+    // import IncomeUpdate from '@/components/transaction/IncomeUpdate.vue';
 </script>
 
 <style>
